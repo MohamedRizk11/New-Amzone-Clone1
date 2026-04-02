1a6cd28e96f1398fca3d56e418ac3af762d0a930
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2026-04-02 16:15:49 +0200

    add related items & product slug

--- product/models.py
+++ product/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+from django.utils import timezone 
+from django.utils.text import slugify 
 from django.utils.translation import gettext_lazy as _
 from taggit.managers import TaggableManager
+
 FLag_types=(
 ('Sale','Sale'),
 ('New','New'),
@@ -20,9 +22,14 @@ class Product (models.Model):
     quantity= models.IntegerField(_('Quantity'),)
     brand=models.ForeignKey('Brand',verbose_name=_('Brand'), related_name='product_brand', on_delete=models.SET_NULL,null=True)
     tags = TaggableManager()
+    slug=models.SlugField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+    
+    def save(self, *args, **kwargs):
+       self.slug=slugify(self.name)
+       super(Product, self).save(*args, **kwargs) # Call the real save() method
     
 class Productimages (models.Model):
     product=models.ForeignKey(Product,verbose_name=_('Product'),related_name='product_image',on_delete=models.CASCADE)
--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,5 @@ from .views import Producdetail , Productlist
 
 urlpatterns = [
     path('',Productlist.as_view() ),
-    path('<int:pk>',Producdetail.as_view() ),
+    path('<slug:slug>',Producdetail.as_view() ),
 ]
