e4716d7f6196986e73f453874a8744666f7dff89
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2026-04-19 14:21:08 +0200

    f

--- project/settings.py
+++ project/settings.py
@@ -41,8 +41,12 @@ INSTALLED_APPS = [
     'product',
     "debug_toolbar",
     'setting',
+    "rest_framework",
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
