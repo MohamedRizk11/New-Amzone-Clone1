a1e445363ce8608dd09d7161216af210c631bba8
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2026-05-23 23:42:24 +0300

    add cart ttal

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView 
+from .models import order
 
-# Create your views here.
+
+
+class orderlist(ListView):
+    model= order
+
+    def get_queryset(self):
+        queryset = super().get_queryset().filter(user=self.request.user)
+
+        return queryset
